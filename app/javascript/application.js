264bd0434ef010888e9d1d448fa720b903338347
Mauro Almiron <mdavid.almiron@gmail.com>
2024-07-08 14:31:44 -0300

    Add a component to test if React is work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import HelloMessage from './components/App';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(<HelloMessage name="World" />);
+});
